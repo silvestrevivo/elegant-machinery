1712442fb3e513539a1ca497d1cd22fa0b8f475e
silvestrevivo <silvestrevivomillan@gmail.com>
2017-05-10 11:48:12 +0200

    third version

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,26 @@
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('play');
-  setTimeout(function(){
-    key.classList.remove('play');
-  }, 100);
-});
+window.onload = function(){
+  //fadeIn Efect
+  var keyLi = document.querySelectorAll('.key');
+  var showUp = function(element){
+    element.classList.add('visible');
+    setTimeout(function(){
+      var next = element.nextElementSibling;
+      showUp(next);
+    }, 100);
+  };
+  showUp(keyLi[0]);
+};
+
+setTimeout(function(){
+  window.addEventListener('keydown', function(e){
+    const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+    const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('play');
+    setTimeout(function(){
+      key.classList.remove('play');
+    }, 100);
+  });
+}, 1200);
